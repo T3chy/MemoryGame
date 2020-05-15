acf58fcdd796004c5f0d6f12a9784c2be4fb2f09
GuerdonL <guerdon@ligon.org>
2020-05-14 22:40:33 -0700

    Broken Red

---
+++ Memory Game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float smoother;
+    public Transform player;
+    private Transform cam;
+    private float displacement;
+    private bool isLocked;
+    public bool IsLocked 
+    {
+        get { return isLocked; } 
+        set { isLocked = value; } 
+    }
+
+    private void Start()
+    {
+        cam = GetComponent<Transform>();
+    }
+
+
+
+    void FixedUpdate()
+    {
+        if (isLocked)
+        {
+            displacement = player.position.x - cam.position.x;
+            cam.position = new Vector3(cam.position.x + (displacement * smoother), player.position.y, cam.position.z);
+        }
+        
+    }
+}
