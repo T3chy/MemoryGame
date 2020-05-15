757701ed1abae29c4ecb636285a8281fee6c1a38
GuerdonL <guerdon@ligon.org>
2020-05-15 00:32:36 -0700

    Camera follow and unlock controller. Rebound keys.

---
+++ Memory Game/Assets/CameraFollow.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float smootherX;
+    [Range(0, 1)]
+    public float smootherY;
+    public Transform player;
+    public Transform cam;
+    private float displacement;
+    public bool isLocked;
+    public float camSpeed;
+
+
+    private void Start()
+    {
+        isLocked = true;
+        camSpeed = .2f;
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            isLocked = !isLocked;
+            
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isLocked)
+        {
+            displacement = player.position.x - cam.position.x;
+            cam.position = new Vector3(cam.position.x + (displacement * smootherX), player.position.y + (displacement * smootherY), cam.position.z);
+        }
+        else
+        {
+            cam.position= new Vector3(cam.position.x + Input.GetAxisRaw("Horizontal") * camSpeed, cam.position.y + Input.GetAxisRaw("Vertical") * camSpeed, cam.position.z);
+
+        }
+
+    }
+}
--- Memory Game/Assets/PlayerController2d.cs
+++ Memory Game/Assets/PlayerController2d.cs
@@ -16,41 +16,62 @@ public class PlayerController2d : MonoBehaviour
     public int extraJumpsVal;
     private int extraJumps;
     public Animator animator;
+    private bool controlling;
+    
     // Start is called before the first frame update
     void Start()
     {
+
+        controlling = true;
         extraJumps = extraJumpsVal;
         rb = GetComponent<Rigidbody2D>();
     }
-void Update(){
-    if (isGrounded == true){
-        extraJumps = extraJumpsVal;
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            controlling = !controlling;
+        }
+        if (isGrounded == true)
+        {
+            extraJumps = extraJumpsVal;
+        }
+        if (Input.GetAxisRaw("Vertical") == 1f && extraJumps == 0 && isGrounded == true && controlling)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            Debug.Log("jumpin off da floor");
+        }
+        else if (Input.GetAxisRaw("Vertical") == 1f && extraJumps > 0 && controlling)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
     }
 
-    if(Input.GetKeyDown(KeyCode.W) && extraJumps == 0 && isGrounded == true){
-    rb.velocity = Vector2.up * jumpForce;
-    Debug.Log("jumpin off da floor");
-    }
-    else if(Input.GetKeyDown(KeyCode.W) && extraJumps > 0){
-        rb.velocity = Vector2.up * jumpForce;
-        extraJumps--;
+    void FixedUpdate()
+    {
+        if (controlling)
+        {
+            animator.SetFloat("Speed", Mathf.Abs(moveInput));
+            isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+            moveInput = Input.GetAxis("Horizontal");
+            rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+            if (facingRight == false && moveInput > 0)
+            {
+                Flip();
+            }
+            else if (facingRight == true && moveInput < 0)
+            {
+                Flip();
+            }
+        }
+        
     }
 
 
-}
-    void FixedUpdate(){
-    animator.SetFloat("Speed",Mathf.Abs(moveInput));
-    isGrounded = Physics2D.OverlapCircle(groundCheck.position,checkRadius,whatIsGround);
-    moveInput = Input.GetAxis("Horizontal");
-    rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
-    if(facingRight == false && moveInput > 0){
-        Flip();
-    }
-    else if (facingRight == true && moveInput <0){
-        Flip();
-    }
-    }
-    void Flip(){
+    void Flip()
+    {
         facingRight = !facingRight;
         Vector3 Scaler = transform.localScale;
         Scaler.x *= -1;
