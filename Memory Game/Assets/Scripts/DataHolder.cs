21f9c1f313c7e6104f508d8ddf63b50a0271e08b
GuerdonL <guerdon@ligon.org>
2020-05-15 03:10:07 -0700

    Save Mechanism, key remapping, and tutorial levels

---
+++ Memory Game/Assets/Scripts/DataHolder.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataHolder : MonoBehaviour
+{
+    public Transform grid;
+    public Transform player;
+    public Rigidbody2D playerRB;
+    public GameObject black;
+    public GameObject purple;
+    public GameObject red;
+    public GameObject yellow;
+    public GameObject green;
+
+    private Vector3 gridPos;
+    private Vector3 playerPos;
+    private Vector2 playerVel;
+    private bool blackOn;
+    private bool purpleOn;
+    private bool redOn;
+    private bool yellowOn;
+    private bool greenOn;
+    
+
+    public void Save() {
+        gridPos = grid.position;
+        playerPos = player.position;
+        playerVel = playerRB.velocity;
+        blackOn = black.activeSelf;
+        purpleOn = purple.activeSelf;
+        redOn = red.activeSelf;
+        yellowOn = yellow.activeSelf;
+        greenOn = green.activeSelf;
+    }
+
+    public void Load() {
+        grid.position = gridPos;
+        player.position = playerPos;
+        playerRB.velocity = playerVel;
+        black.SetActive(blackOn);
+        purple.SetActive(purpleOn);
+        red.SetActive(redOn);
+        yellow.SetActive(yellowOn);
+        green.SetActive(greenOn);
+    }
+}
---
+++ Memory Game/Assets/Scripts/clickToForget.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+public class clickToForget : MonoBehaviour {
+
+
+    public Animator animator;
+    public GameObject color;
+
+
+    void OnMouseDown()
+    {
+                animator.SetBool("Deleting",true);
+                Thread.Sleep(500);
+                color.SetActive(false);
+                        Thread.Sleep(500);
+                
+        animator.SetBool("Deleting",false);     
+    }
+}
