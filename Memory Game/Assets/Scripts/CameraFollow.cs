1d50ed7b155118d63aa436243a3e8458d9a725f7
GuerdonL <guerdon@ligon.org>
2020-05-16 06:12:01 -0700

    ported the full project into a new unity setup in order to fix movement. all compatabilility is broken.

--- Memory Game/Assets/Scripts/CameraFollow.cs
+++ Memory Game/Assets/Scripts/CameraFollow.cs
@@ -1,48 +1,122 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class CameraFollow : MonoBehaviour
-{
-    [Range(0, 1)]
-    public float smootherX;
-    [Range(0, 1)]
-    public float smootherY;
-    public Transform player;
-    public Transform cam;
-    private float displacement;
-    public bool isLocked;
-    public float camSpeed;
+public class CameraFollow : MonoBehaviour {
+    
+	public Controller2D target;
+	public float verticalOffset;
+	public float lookAheadDstX;
+	public float lookSmoothTimeX;
+	public float verticalSmoothTime;
+	public Vector2 focusAreaSize;
 
+	FocusArea focusArea;
 
-    private void Start()
-    {
+	float currentLookAheadX;
+	float targetLookAheadX;
+	float lookAheadDirX;
+	float smoothLookVelocityX;
+	float smoothVelocityY;
+
+	bool lookAheadStopped;
+
+    private bool isLocked;
+    private Transform cam;
+	void Start() {
         isLocked = true;
-        camSpeed = .2f;
-
-    }
-
-    private void Update()
+        cam = GetComponent<Transform>();
+		focusArea = new FocusArea (target.GetComponent<BoxCollider2D>().bounds, focusAreaSize);
+	}
+    void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
+        if (Input.GetKeyDown(KeyCode.Z)) {
             isLocked = !isLocked;
-            
         }
     }
-
-    void FixedUpdate()
+    private void FixedUpdate()
     {
+        if (!isLocked)
+        {
+            cam.position = new Vector3(cam.position.x + Input.GetAxis("Horizontal"), cam.position.y + Input.GetAxis("Vertical"), cam.position.z);
+        }
+    }
+    void LateUpdate() {
         if (isLocked)
         {
-            displacement = player.position.x - cam.position.x;
-            cam.position = new Vector3(cam.position.x + (displacement * smootherX), player.position.y + (displacement * smootherY), cam.position.z);
-        }
-        else
-        {
-            cam.position= new Vector3(cam.position.x + Input.GetAxisRaw("Horizontal") * camSpeed, cam.position.y + Input.GetAxisRaw("Vertical") * camSpeed, cam.position.z);
+            focusArea.Update(target.GetComponent<BoxCollider2D>().bounds);
 
-        }
+            Vector2 focusPosition = focusArea.centre + Vector2.up * verticalOffset;
 
-    }
+            if (focusArea.velocity.x != 0)
+            {
+                lookAheadDirX = Mathf.Sign(focusArea.velocity.x);
+                if (Mathf.Sign(target.playerInput.x) == Mathf.Sign(focusArea.velocity.x) && target.playerInput.x != 0)
+                {
+                    lookAheadStopped = false;
+                    targetLookAheadX = lookAheadDirX * lookAheadDstX;
+                }
+                else
+                {
+                    if (!lookAheadStopped)
+                    {
+                        lookAheadStopped = true;
+                        targetLookAheadX = currentLookAheadX + (lookAheadDirX * lookAheadDstX - currentLookAheadX) / 4f;
+                    }
+                }
+            }
+
+
+            currentLookAheadX = Mathf.SmoothDamp(currentLookAheadX, targetLookAheadX, ref smoothLookVelocityX, lookSmoothTimeX);
+
+            focusPosition.y = Mathf.SmoothDamp(transform.position.y, focusPosition.y, ref smoothVelocityY, verticalSmoothTime);
+            focusPosition += Vector2.right * currentLookAheadX;
+            transform.position = (Vector3)focusPosition + Vector3.forward * -10;
+        }
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = new Color (1, 0, 0, .5f);
+		Gizmos.DrawCube (focusArea.centre, focusAreaSize);
+	}
+
+	struct FocusArea {
+		public Vector2 centre;
+		public Vector2 velocity;
+		float left,right;
+		float top,bottom;
+
+
+		public FocusArea(Bounds targetBounds, Vector2 size) {
+			left = targetBounds.center.x - size.x/2;
+			right = targetBounds.center.x + size.x/2;
+			bottom = targetBounds.min.y;
+			top = targetBounds.min.y + size.y;
+
+			velocity = Vector2.zero;
+			centre = new Vector2((left+right)/2,(top +bottom)/2);
+		}
+
+		public void Update(Bounds targetBounds) {
+			float shiftX = 0;
+			if (targetBounds.min.x < left) {
+				shiftX = targetBounds.min.x - left;
+			} else if (targetBounds.max.x > right) {
+				shiftX = targetBounds.max.x - right;
+			}
+			left += shiftX;
+			right += shiftX;
+
+			float shiftY = 0;
+			if (targetBounds.min.y < bottom) {
+				shiftY = targetBounds.min.y - bottom;
+			} else if (targetBounds.max.y > top) {
+				shiftY = targetBounds.max.y - top;
+			}
+			top += shiftY;
+			bottom += shiftY;
+			centre = new Vector2((left+right)/2,(top +bottom)/2);
+			velocity = new Vector2 (shiftX, shiftY);
+		}
+	}
+
 }
---
+++ Memory Game/Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Player))]
+public class PlayerInput : MonoBehaviour {
+
+	Player player;
+    private bool isLocked;
+    private Transform draw;
+    DataHolder database;
+    private Vector2 velocity;
+
+    void Start () {
+        isLocked = true;
+        draw = GetComponent<Transform>();
+		player = GetComponent<Player> ();
+        database = GameObject.Find("SaveState").GetComponent<DataHolder>();
+    }
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            isLocked = !isLocked;
+        }
+        if (isLocked)
+        {
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                velocity = player.SaveVelocity();
+                database.Save();
+            }
+            else if (Input.GetKeyDown(KeyCode.C) && velocity == null)
+            {
+                player.SetVelocity(velocity);
+                database.Load();
+            }
+            Vector2 directionalInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            if (Input.GetAxisRaw("Horizontal") != 0)
+            {
+                draw.localScale = new Vector3(Mathf.Abs(draw.localScale.x) * Input.GetAxisRaw("Horizontal"), draw.localScale.y, draw.localScale.z);
+            }
+            player.SetDirectionalInput(directionalInput);
+
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                player.OnJumpInputDown();
+            }
+            if (Input.GetKeyUp(KeyCode.UpArrow))
+            {
+                player.OnJumpInputUp();
+            }
+        }
+	}
+}
