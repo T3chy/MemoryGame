26e0e4fc35a3424f68ff910cbdd0c23a0679d278
t3chy <elam.day.friedland@gmail.com>
2020-05-16 22:36:21 -0700

    perliminary health system just text for now

--- Memory Game/Assets/Scripts/Bullet Hell/hitDetection.cs
+++ Memory Game/Assets/Scripts/Bullet Hell/hitDetection.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class hitDetection : MonoBehaviour
 {
-private void OnTriggerEnter2D(Collider2D other){
-Debug.Log("hit!");
+    int startHealth;
+    public Text healthtext;
+    void Start(){
+    startHealth = 5 - forgotten.forgottenList.Count;
+    healthtext.text = startHealth.ToString();
+    }
+    private void OnTriggerEnter2D(Collider2D other){
+Destroy(other);
+ startHealth -= 1;
+ healthtext.text = startHealth.ToString();
 }
 }
