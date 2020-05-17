8a87cdbdeb32579d0ad0cb27f5db68802b9e12b1
t3chy <elam.day.friedland@gmail.com>
2020-05-16 23:30:09 -0700

    finished bullet hell framework

---
+++ Memory Game/Assets/Scripts/Bullet Hell/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Timer : MonoBehaviour
+{
+    public Animator transition;
+    public Text timeRemainingText;
+    float timeLeft;
+    void Start()
+    {
+        timeLeft = 20 + (20 * forgotten.forgottenList.Count);
+        timeRemainingText.text = timeLeft.ToString() + " sec";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        timeRemainingText.text = timeLeft.ToString() + " sec";
+        if (timeLeft < 0) {
+        LevelLoader.LoadLevel(transition);
+        }
+    }
+}
